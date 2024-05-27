110c4a5a117b0c60943c917a86e9a5b59b80763d
safi-is-coding <cse.mohammadsafi@gmail.com>
2024-05-27 15:02:46 +0530

    Accordian

---
+++ Accordian/script.js
@@ -0,0 +1,25 @@
+const cont = document.querySelector('.contents')
+
+function show1(){
+    if(document.querySelector('.dp1').style.display == 'none'){
+        document.querySelector('.dp1').style.display = 'block'
+    }else{
+        document.querySelector('.dp1').style.display = 'none'
+    }
+}
+
+function show2(){
+    if(document.querySelector('.dp2').style.display == 'none'){
+        document.querySelector('.dp2').style.display = 'block'
+    }else{
+        document.querySelector('.dp2').style.display = 'none'
+    }
+}
+
+function show3(){
+    if(document.querySelector('.dp3').style.display == 'none'){
+        document.querySelector('.dp3').style.display = 'block'
+    }else{
+        document.querySelector('.dp3').style.display = 'none'
+    }
+}
