2a752fa693c372f1e53d31412838f401fafece87
safi-is-coding <cse.mohammadsafi@gmail.com>
2024-06-01 13:06:28 +0530

    ecommerce website

---
+++ E Cart Website/script.js
@@ -0,0 +1,71 @@
+        // <div class="card">
+        //     <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="image">
+        //     <div class="details">
+        //         <h5 id="title">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h5>
+        //         <h4 id="price">$ 109.95</h4>
+        //         <p id="description">Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
+        //         <p id="rating">Rating : <b>3.9</b></p>
+        //     </div>
+        // </div>
+
+
+
+
+const container = document.querySelector('.container')
+const card = document.querySelector('.card')
+const details = document.querySelector('.details')
+const title = document.querySelector('#title')
+const price = document.querySelector('#price')
+const description = document.querySelector('#description')
+const rating = document.querySelector('#rating')
+const category = document.querySelector('#category')
+
+
+const fetchData = async (categoryType) => {
+    const response = await fetch('https://fakestoreapi.com/products')
+
+    const data = await response.json()
+
+    // console.log(data);
+
+    container.innerHTML = ''
+    for (i=0; i<data.length; i++){
+        
+        if(data[i].category === categoryType){
+            container.innerHTML += `
+                <div class="card">
+                    <img src="${data[i].image}" alt="image">
+                    <div class="details">
+                        <h5 id="title">${data[i].title}</h5>
+                        <h4 id="price">$ ${data[i].price}</h4>
+                        <p id="description">${data[i].description}</p>
+                        <p id="rating">Rating : <b>${data[i].rating.rate}</b>&nbsp;&nbsp;&nbsp;Count : <b>${data[i].rating.count}</b></p>
+                    </div>
+                </div>
+                `
+        }
+        else if(categoryType === 'show all'){
+            container.innerHTML += `
+                <div class="card">
+                    <img src="${data[i].image}" alt="image">
+                    <div class="details">
+                        <h5 id="title">${data[i].title}</h5>
+                        <h4 id="price">$ ${data[i].price}</h4>
+                        <p id="description">${data[i].description}</p>
+                        <p id="rating">Rating : <b>${data[i].rating.rate}</b>&nbsp;&nbsp;&nbsp;Count : <b>${data[i].rating.count}</b></p>
+                    </div>
+                </div>
+                `
+        }
+        
+    }
+}
+
+window.onload(fetchData('show all'))
+
+function displayCategory(){
+    // console.log(category.value);
+
+    fetchData(category.value)
+}
+
--- Pokemon Api/script.js
+++ Pokemon Api/script.js
@@ -1,5 +1,6 @@
 const pokemonName = document.querySelector('#pokemonName');
 const container = document.querySelector('.container');
+const filterContainer = document.querySelector('.filterContainer');
 const searchBtn = document.querySelector('#search');
 const api = "https://pokeapi.co/api/v2/pokemon/?limit=200";
 let URL = '';
@@ -8,6 +9,9 @@ const fetchData = async () => {
     try {
         const response = await fetch(api);
         const data = await response.json();
+
+        container.style.display = 'flex'
+        filterContainer.style.display = 'none'
 
         data.results.forEach((pokemon, index) => {
             container.innerHTML += `
@@ -29,13 +33,45 @@ const fetchData = async () => {
     }
 };
 
+const fetchFilterData = async (pname) => {
+    try {
+        const response = await fetch(api);
+        const data = await response.json();
 
-// pokemonName.addEventListener("keypress", function (event) {
-//     if (event.key === "Enter") {
-//         console.log('clicked');
-//         fetchFilterData(pokemonName.value.toLowerCase());
-//     }
-// });
+        // console.log(data);
+
+        data.results.forEach((pokemon, index) => {
+
+            if(pokemon.name.toLowerCase() === pname){
+                
+                container.style.display = 'none'
+                filterContainer.style.display = 'flex'
+                filterContainer.innerHTML += `
+                
+                <div class="card" onclick='getUrl("https://pokeapi.co/api/v2/pokemon/${index+1}/")'>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index+1}.svg" alt="pokemon image" id="pokemonImage">
+                    <div class="cardDetails">
+                        <p id="pokemonType">#${index+1}</p> 
+                        <h4 id="charName">${pokemon.name.toUpperCase()}</h4>
+                    </div>
+                </div>
+            `;
+            }
+            
+        });
+
+    } catch (error) {
+        filterContainer.innerHTML = `<p style="color:red;">${error}</p>`;
+    }
+};
+
+
+pokemonName.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        console.log('clicked');
+        fetchFilterData(pokemonName.value.toLowerCase());
+    }
+});
 
 
 
@@ -49,7 +85,9 @@ function getUrl(u) {
 }
 
 
-
+searchBtn.addEventListener('click', ()=> {
+    fetchFilterData(pokemonName.value.toLowerCase());
+})
 
 fetchData();
 
